947e8d570d852b73da8507281f699cd84f11732b
dnc073 <67540067+dnc073@users.noreply.github.com>
2020-06-28 17:31:53 +0700

    Add files via upload

---
+++ bot.sh
@@ -0,0 +1,9 @@
+python main.py +6285156407581 ltc
+sleep 2
+python main.py +6285156407581 btc
+sleep 2
+python main.py +6285156407581 bch
+sleep 2
+python main.py +6285156407581 doge
+sleep 2
+sh bot.sh
